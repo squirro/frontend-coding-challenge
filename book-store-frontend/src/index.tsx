cf2f04d52aa37b556df12552e6784869301ecaa5
Simon Harte <simon.harte@protonmail.com>
2022-04-28 08:35:26 +0200

    add css reset

--- book-store-frontend/src/index.tsx
+++ book-store-frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import 'the-new-css-reset';
 import { store } from './app/store';
 import App from './App';
 import './styles/index.scss';
